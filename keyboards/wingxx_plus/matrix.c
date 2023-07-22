be67834be5e0995d8ef53a3ecb71d9903a412531
f14eagle <f14eagle@gmail.com>
2023-07-22 22:57:14 +0800

    fix wingxx matrix.c

--- keyboards/wingxx_plus/matrix.c
+++ keyboards/wingxx_plus/matrix.c
@@ -24,6 +24,8 @@
 #include "util.h"
 #include "matrix.h"
 #include "timer.h"
+#include "debounce.h"
+#include "quantum.h"
 
 #if (MATRIX_COLS <= 8)
 #    define print_matrix_header()  print("\nr/c 01234567\n")
@@ -42,7 +44,11 @@
 #    define ROW_SHIFTER  ((uint32_t)1)
 #endif
 
+static const pin_t row_pins[MATRIX_ROWS] = MATRIX_ROW_PINS;
+static const pin_t col_pins[MATRIX_COLS] = MATRIX_COL_PINS;
+
 /* matrix state(1:on, 0:off) */
+static matrix_row_t raw_matrix[MATRIX_ROWS];  // raw values
 static matrix_row_t matrix[MATRIX_ROWS];
 
 __attribute__ ((weak))
@@ -83,13 +89,75 @@ uint8_t matrix_cols(void) {
     return MATRIX_COLS;
 }
 
+inline
+uint8_t pad_start_col(void){
+    return PAD_START_COL;
+}
+
+static void select_row(uint8_t row) {
+    setPinOutput(row_pins[row]);
+    writePinLow(row_pins[row]);
+}
+
+static void unselect_row(uint8_t row) { setPinInputHigh(row_pins[row]); }
+
+static void unselect_rows(void) {
+    for (uint8_t x = 0; x < MATRIX_ROWS; x++) {
+        setPinInputHigh(row_pins[x]);
+    }
+}
+
+static void init_pins(void) {
+    unselect_rows();
+    for (uint8_t x = PAD_START_COL; x < MATRIX_COLS; x++) {
+        setPinInputHigh(col_pins[x]);
+    }
+}
+
+static bool read_cols_on_row(matrix_row_t current_matrix[], uint8_t current_row) {
+    // Store last value of row prior to reading
+    matrix_row_t last_row_value = current_matrix[current_row];
+
+    // Clear data in matrix row
+    current_matrix[current_row] = 0;
+
+    // Select row and wait for row selecton to stabilize
+    select_row(current_row);
+    wait_us(30);
+
+    // For each col...
+    for (uint8_t col_index = PAD_START_COL; col_index < MATRIX_COLS; col_index++) {
+        // Select the col pin to read (active low)
+        uint8_t pin_state = readPin(col_pins[col_index]);
+
+        // Populate the matrix row with the state of the col pin
+        current_matrix[current_row] |= pin_state ? 0 : (ROW_SHIFTER << col_index);
+    }
+
+    // Unselect row
+    unselect_row(current_row);
+
+    return (last_row_value != current_matrix[current_row]);
+}
+
 void matrix_init(void) {
+    // initialize key pins
+    init_pins();
+
+    // initialize matrix state: all keys off
+    for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
+        raw_matrix[i] = 0;
+        matrix[i]     = 0;
+    }
+
+    debounce_init(MATRIX_ROWS);
 
     matrix_init_quantum();
 }
 
 uint8_t matrix_scan(void)
 {
+    // Redox scan logic
     SERIAL_UART_INIT();
 
     uint32_t timeout = 0;
@@ -119,11 +187,18 @@ uint8_t matrix_scan(void)
     if (uart_data[10] == 0xE0)
     {
         //shifting and transferring the keystates to the QMK matrix variable
+        // 0x3C000 = pad mask
         for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
-            matrix[i] = (uint16_t) uart_data[i*2] | (uint16_t) uart_data[i*2+1] << 7;
+            matrix[i] = (uint16_t) uart_data[i*2] | (uint16_t) uart_data[i*2+1] << 7 | (matrix[i] & 0x3C000);
         }
     }
 
+    // Wire logic
+    bool changed = false;
+    for (uint8_t current_row = 0; current_row < MATRIX_ROWS; current_row++) {
+        changed |= read_cols_on_row(raw_matrix, current_row);
+    }
+    debounce(raw_matrix, matrix, MATRIX_ROWS, changed);
 
     matrix_scan_quantum();
     return 1;
@@ -146,8 +221,7 @@ void matrix_print(void)
     print_matrix_header();
 
     for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
-        //phex(row);
-        print(": ");
+        phex(row); print(": ");
         print_matrix_row(row);
         print("\n");
     }
